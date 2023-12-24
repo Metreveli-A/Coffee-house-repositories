c229900265815df33e0661ca5792ae826e077fb9
Metreveli-A <a.m.facke@gmail.com>
2023-12-25 00:26:46 +0400

    this is coffee house with repository

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ The layout matches the design +42
 CSS Requirements +10
 Interactivity +32
 `);
+const humburger = document.querySelector('.humburger')
+const ul = document.querySelector('ul')
+
+humburger.addEventListener('click',()=>{
+    ul.classList.toggle('ham_active')
+})
